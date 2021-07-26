be16dfa4964686f5f9d4fe07c310b266842b887f
Jackie Chen <jch401@uclive.ac.nz>
2021-07-26 14:30:48 +1200

    Initial commit with skeleton layout

---
+++ src/components/landing.js
@@ -0,0 +1,10 @@
+import {Container} from "@material-ui/core";
+
+export default function Landing() {
+    return (
+      <Container>
+          <h1> Jackie Chen </h1>
+          <p> Computer & Data Scientist, Powerlifter, Gamer, Footballer </p>
+      </Container>
+    );
+}
