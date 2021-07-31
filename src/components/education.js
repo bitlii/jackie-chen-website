f15da95166a6a104db373b0f2cb5c421c0256620
Jackie Chen <jch401@uclive.ac.nz>
2021-08-01 00:27:02 +1200

    Additional animation for subtext. Expanded education section.

--- src/components/education.js
+++ src/components/education.js
@@ -1,10 +1,49 @@
-import {Container} from "semantic-ui-react";
+import {Container, Card, Icon} from "semantic-ui-react";
 
 export default function Education() {
+    const educationCardStyle = {
+        maxWidth: "500px",
+    }
+    // todo: extract this css into scss file.
+    const header = {
+        display: "flex",
+        justifyContent: "center",
+    }
+
     return (
       <Container id="education" className="section">
-          <h2>Education</h2>
-          <p>Recent Graduate from UC.</p>
+          <div style={header}>
+              <Icon className="graduation cap" style={{margin: "auto 4px auto 0"}} size="large"/>
+              <h2 style={{margin: "auto 0"}}>Education</h2>
+          </div>
+          <Card
+              style={educationCardStyle}
+              centered
+              raised
+              fluid>
+              <Card.Content>
+                  <Card.Header textAlign="left">
+                      University of Canterbury
+                  </Card.Header>
+                  <Card.Meta>
+                      2019 - 2021
+                  </Card.Meta>
+                  <Card.Meta>
+                      7.11 GPA
+                  </Card.Meta>
+                  <Card.Description>
+                      Bachelor of Science: Computer Science and Data Science
+                  </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                  <Card.Description>
+                      UC Football Club General Executive Committee Member
+                  </Card.Description>
+                  <Card.Meta>
+                      2020 - 2021
+                  </Card.Meta>
+              </Card.Content>
+          </Card>
       </Container>
     );
 }
--- src/components/landing.js
+++ src/components/landing.js
@@ -6,17 +6,36 @@ import {Button, Container, Flag, Icon} from "semantic-ui-react";
 export default function Landing() {
     let bottomSection = createRef();
 
+    // Scroll to fade v button.
     const {scrollY} = useViewportScroll();
-    const fade = useTransform(scrollY, [0, window.innerHeight/1.5], [1, 0]);
+    const fade = useTransform(scrollY, [0, window.innerHeight/3], [1, 0]);
 
+    // Subtext fade-in display animation.
+    const subtextContainer = {
+        hidden: {opacity: 0},
+        show: {
+            opacity: 1,
+            transition: {
+                delayChildren: 1,
+                staggerChildren: 1
+            }
+        }
+    }
+
+    const subtextVariants = {
+        hidden: {opacity: 0},
+        show: {opacity: 1},
+    }
+
+    // Scroll to the first section.
     const scrollToNextSection = () => bottomSection.scrollIntoView({behavior: "smooth", alignToTop: false});
 
     return (
         <Container id="home" className="landing-section section">
             <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ y: [100, 0], opacity: 1 }}
-                transition={{ ease: "easeOut", duration: 2 }}
+                initial={{opacity: 0}}
+                animate={{y: [100, 0], opacity: 1}}
+                transition={{ease: "easeOut", duration: 3, staggerChildren: 2}}
                 className="header">
                     <h1 id="name">Jackie Chen</h1>
                     <div>
@@ -24,11 +43,35 @@ export default function Landing() {
                         <Flag name="cn"/>
                     </div>
                     <br/>
-                    <p> Computer & Data Scientist, Powerlifter, Gamer, Footballer </p>
+                    <motion.p
+                        variants={subtextContainer}
+                        initial="hidden"
+                        animate="show">
+                        <motion.span
+                            variants={subtextVariants}
+                            className="subtext">
+                            Computer Scientist,
+                        </motion.span>
+                        <motion.span
+                            variants={subtextVariants}
+                            className="subtext">
+                            Data Scientist,
+                        </motion.span>
+                        <motion.span
+                            variants={subtextVariants}
+                            className="subtext">
+                            Powerlifter,
+                        </motion.span>
+                        <motion.span
+                            variants={subtextVariants}
+                            className="subtext">
+                            Gamer.
+                        </motion.span>
+                    </motion.p>
             </motion.div>
 
             <motion.div
-                style={{ opacity: fade }}
+                style={{opacity: fade}}
                 id="bottom-section"
                 ref={el => bottomSection = el}>
                 <Button icon basic inverted circular onClick={() => scrollToNextSection()}>
